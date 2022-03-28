0553b13f777342c13b4259f8419240b3e1d4a944
antcorper <rubio994@hotmail.com>
2022-03-28 18:52:56 +0200

    login html css

---
+++ index.js
@@ -0,0 +1,9 @@
+//boton set
+localStorage.setItem("fullname", "Charles Widmore");
+
+//boton get
+const fullname = localStorage.getItem("fullname");
+console.log(fullname);
+
+//boton remove
+localStorage.removeItem("fullname");
